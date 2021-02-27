d425b4f5096417efabe05e12bc90d4ebe5a3247e
Adem OKUR <adem.okur5734@gmail.com>
2021-02-27 22:33:22 +0200

    updates.1

--- src/Lesson_5.java
+++ src/Lesson_5.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
 
-public class Lesson_5
+public class Lesson_5 {
+    public static void main(String[] args) {
+        System.out.println("Gun Adını Yazınız:");
+        Scanner scanner = new Scanner(System.in);
+        String gun = scanner.next();
 
-{
 
 
 
+        switch (gun.toLowerCase()) {
+            case "cuma":
+                System.out.println("Müslümanlar için kutsal gün.");
+                break;
+            case "Cumartesi":
+                System.out.println("Yahudiler için kutsal gün.");
+                break;
+            case "Pazar":
+                System.out.println("Hristiyanlar için kutsal gün.");
+                break;
+            default:
+                System.out.println("Kutsal gün degil.");
+
+        }
 
 
+    }
 }
---
+++ src/Lesson_8.java
@@ -0,0 +1,63 @@
+public class Lesson_8 {
+    /*public static void main(String[] args) {
+        int[] dizi = new int[4];
+
+        //birinci elemana deger atayalım
+        dizi[0] = 3;
+        //ikinci elemana deger atayalım
+        dizi[1] = 4;
+        //ucuncu elemana deger atayalım
+        dizi[2] = 9;
+        //dorduncu elemana deger atayalım
+        dizi[3] = 10;
+        System.out.println("dizinin 1.elemanın degeri " + dizi[0]);
+        System.out.println("dizinin 2.elemanın degeri " + dizi[1]);
+        System.out.println("dizinin 3.elemanın degeri " + dizi[2]);
+        System.out.println("dizinin 4.elemanın degeri " + dizi[3]);
+    }
+
+    public static void main(String[] args) {
+        String[] dizi2 = new String[4];
+        dizi2[0] = "birinci eleman";
+        dizi2[1] = "ikinci eleman";
+        dizi2[2] = "uçuncu eleman";
+        dizi2[3] = "dördöncu eleman";
+
+        System.out.println("birinci eleman  " + dizi2[0]);
+        System.out.println("ikinci eleman  " + dizi2[1]);
+        System.out.println("ucuncu eleman  " + dizi2[2]);
+        System.out.println("dörduncu eleman  " + dizi2[3]);
+
+
+    }
+
+    public static void main(String[] args) {
+        String[] dizi2 = {"bir", "iki", "uc", "dört"};
+        int dizi2_uzunluk = dizi2.length;
+        System.out.println(dizi2_uzunluk);
+
+        System.out.println("bir " + dizi2[0]);
+        System.out.println("iki " + dizi2[1]);
+        System.out.println("uc " + dizi2[2]);
+        System.out.println("dort  " + dizi2[3]);
+    }
+
+    public static void main(String[] args) {
+        String[] dizi2 = {"bir", "iki", "uc", "dört"};
+        for (int i = 0; i < dizi2.length; i++) {
+            System.out.println((i + 1) + ".ci eleman: " + dizi2[i]);
+        }
+        public static void main (String[]args){
+            String[] birinci_array = {"bir", "iki", "uc", "dört"};
+            String[] ikinci_array = new String[4];
+
+            ikinci_array = birinci_array.clone();
+
+            for (int i = 0; i < ikinci_array.length; i++) {
+                System.out.println((i + 1) + ".ci eleman:" + ikinci_array[i]);
+
+
+            }
+        }
+    }*/
+}
