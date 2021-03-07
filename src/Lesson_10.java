be2a982704c359267cf054d6b64e2c9d882e8b93
Adem OKUR <adem.okur5734@gmail.com>
2021-03-07 23:38:10 +0200

    hasmap

---
+++ src/Lesson_10.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lesson_10 {
+    /*  public static void main(String[] args) {
+
+      int i=1;
+        while (i<6){
+            System.out.println("Hello");
+            i++;
+
+        int i=10;
+        while (i>=1){
+            System.out.println(i);
+            i--;
+        int i = 1;
+        int sum = 0;
+        while (i <= 10) {
+            sum += i;
+            System.out.println(sum);
+            i++;
+        int arr[]={2,11,45,9};
+        int i=0;
+        while (i< arr.length){
+            System.out.println(arr[i]);
+            i++;
+
+        }*/
+    public static void main(String[] args) {
+       /* int baslangıc_sayısı=1;
+
+
+        do{
+            System.out.println(baslangıc_sayısı);
+            baslangıc_sayısı++;
+
+        }while (baslangıc_sayısı<11);
+
+        int sayı = 10;
+        do {
+            System.out.println(sayı);
+            sayı--;
+            sayı--;
+            //sayı -= 2;
+        } while (sayı > 0);
+        int i = 4;
+        int cift = 0;
+        int tek = 0;
+        do {
+            if (i % 2 == 0) {
+                cift += i;
+            } else {
+                tek += i;
+            }
+            i++;
+        } while (i < 21);
+        System.out.println("çiftler: "+cift);
+        System.out.println("tekler: "+tek);*/
+
+
+    }
+}
+
+
+
--- src/Lesson_11.java
+++ src/Lesson_11.java
@@ -1,67 +1,79 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Lesson_11 {
-    /*  public static void main(String[] args) {
-
-      int i=1;
-        while (i<6){
-            System.out.println("Hello");
-            i++;
-
-        int i=10;
-        while (i>=1){
-            System.out.println(i);
-            i--;
-        int i = 1;
-        int sum = 0;
-        while (i <= 10) {
-            sum += i;
-            System.out.println(sum);
-            i++;
-        int arr[]={2,11,45,9};
-        int i=0;
-        while (i< arr.length){
-            System.out.println(arr[i]);
-            i++;
-
-        }*/
     public static void main(String[] args) {
-       /* int baslangıc_sayısı=1;
+      /*    HashMap<Integer, String> myMap = new HashMap<>();
+
+        myMap.put(1, "Pazartesi");
+        myMap.put(2, "Salı");
+        myMap.put(3, "Carsamba");
+        myMap.put(4, "Persembe");
+        myMap.put(5, "Cuma");
+        myMap.put(6, "Cumartesi");
+        myMap.put(7, "Pazar");
+
+      boolean bir_iceriyormu=myMap.containsKey(1);
+        boolean sekiz_iceriyormu=myMap.containsKey(8);
+
+        System.out.println("bir iceriyor mu:"+bir_iceriyormu);
+        System.out.println("sekiz iceriyor mu:"+sekiz_iceriyormu);
+
+        boolean salı_iceriyormu= myMap.containsValue("Salı");
+        boolean hafta_iceriyormu= myMap.containsValue("Hafta");
+
+        System.out.println("salı iceriyor mu:"+salı_iceriyormu);
+        System.out.println("hafta iceriyor mu:"+hafta_iceriyormu);
+
+        //key=4 olan entry i kaldıralım
+        myMap.remove(4);
+        System.out.println(myMap);
+
+        myMap.remove(2,"Salı");
+        System.out.println(myMap);
+
+        myMap.replace(2,"SALI");
+        System.out.println(myMap);
+
+        for (Integer key:myMap.keySet()
+             ) {
+            System.out.println(key);
+        for (String value:myMap.values()
+             ) {
+            System.out.println(value);
+
+        }
+        for (Map.Entry mapElemanı:myMap.entrySet()
+             ) {
+            System.out.print(mapElemanı.getKey()+":");
+            System.out.print(mapElemanı.getValue());
+        }*/
+        HashMap<String, String> myMap = new HashMap<>();
+        myMap.put("tr", "Turkısh");
+        myMap.put("en", "England");
+        myMap.put("de", "German");
+        myMap.put("ar", "Arabic");
+
+        boolean tr_varmı = myMap.containsKey("tr");
+        boolean es_varmı = myMap.containsKey("es");
+        System.out.println(tr_varmı);
+        System.out.println(es_varmı);
+
+        for (String key : myMap.keySet()
+        ) {
+            System.out.println(key);
+        }
+        for (String value : myMap.values()
+        )
+        {
+            System.out.println(value);
+        }
 
 
-        do{
-            System.out.println(baslangıc_sayısı);
-            baslangıc_sayısı++;
-
-        }while (baslangıc_sayısı<11);
-
-        int sayı = 10;
-        do {
-            System.out.println(sayı);
-            sayı--;
-            sayı--;
-            //sayı -= 2;
-        } while (sayı > 0);
-        int i = 4;
-        int cift = 0;
-        int tek = 0;
-        do {
-            if (i % 2 == 0) {
-                cift += i;
-            } else {
-                tek += i;
-            }
-            i++;
-        } while (i < 21);
-        System.out.println("çiftler: "+cift);
-        System.out.println("tekler: "+tek);*/
 
 
     }
+
+
 }
-
-
 
