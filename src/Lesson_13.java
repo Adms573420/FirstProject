2335ecee993c4048b494ecbf42b02ad5559b344a
Adem OKUR <adem.okur5734@gmail.com>
2021-03-13 19:23:22 +0200

    HashSet

---
+++ src/Lesson_13.java
@@ -0,0 +1,83 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Lesson_13 {
+    public static void main(String args[]) {
+       //HashSet<String> mySet = new HashSet<>();
+       //mySet.add("Mehmet");
+       //mySet.add("BAhattin");
+       //mySet.add("Murat");
+       //mySet.add("Adil");
+
+        //boolean mehmet_iceriyormu=mySet.contains("Mehmet");
+        //boolean ahmet_iceriyormu=mySet.contains("Ahmet");
+        //System.out.println(mehmet_iceriyormu);
+        //System.out.println(ahmet_iceriyormu);
+
+        //mySet.remove("Mehmet");
+        //System.out.println(mySet);
+
+        // Iterator<String> iterator=mySet.iterator();
+        // while(iterator.hasNext()){
+        //     System.out.println(iterator.next());
+
+        // mySet.forEach(item->System.out.println(item));
+
+        // Object[] my_array=mySet.toArray();
+        // for (int i = 0; i < my_array.length; i++) {
+        //     System.out.println((i+1)+".nci eleman:"+my_array[i]);
+        // }
+
+        //int setin_eleman_sayısı= mySet.size();
+        //System.out.println("setin eleman sayısı:"+setin_eleman_sayısı);
+
+        //mySet.clear();
+        //System.out.println(mySet);
+       //HashSet<Integer> sayı = new HashSet<>();
+       //sayı.add(1);
+       //sayı.add(2);
+       //sayı.add(3);
+       //sayı.add(4);
+       //sayı.add(5);
+
+        // int toplam=0;
+//
+        // Iterator<Integer> ıterator=sayı.iterator();
+        // while(ıterator.hasNext()){
+        //     toplam += ıterator.next();
+//
+        // }System.out.println(toplam);
+
+       // HashSet<String> sehirler = new HashSet<>();
+       // sehirler.add("İStanbul");
+       // sehirler.add("Ankara");
+       // sehirler.add("Sinop");
+       // sehirler.add("Denizli");
+//
+       //int sehirlerin_sayısı= sehirler.size();
+       //System.out.println(sehirlerin_sayısı);
+        //HashSet<String> group1=new HashSet<>();
+       // HashSet<String> group2=new HashSet<>();
+
+      //  group1.add("Olıvia");
+      //  group1.add("Mia");
+      //  group1.add("Alexander");
+      //  group1.add("Mason");
+      //  group1.add("James");
+//
+       //group2.add("Joseph");
+       //group2.add("Mia");
+       //group2.add("Mila");
+       //group2.add("Olıvia");
+       //group2.add("James");
+       //group2.add("Charlotteseph");*/
+
+
+
+        
+
+
+    }
+}
+
+
